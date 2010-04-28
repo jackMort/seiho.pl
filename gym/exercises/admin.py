8e3aeb128fadf32c4ea13870ef956a6da61abf9e
Lech Twaróg <lech.twarog@gmail.com>
2010-04-28 21:49:52 +0200

    dodanie modeli admina

--- gym/exercises/admin.py
+++ gym/exercises/admin.py
@@ -1,9 +1,28 @@
 from gym.exercises.models import *
 from django.contrib import admin
 
-admin.site.register( Type )
-admin.site.register( Muscle )
-admin.site.register( ExerciseTemplate )
-admin.site.register( Training )
-admin.site.register( Set )
-admin.site.register( Exercise )
+class TypeAdmin( admin.ModelAdmin ):
+    pass
+
+class MuscleAdmin( admin.ModelAdmin ):
+    pass
+
+class ExerciseTemplateAdmin( admin.ModelAdmin ):
+    pass
+
+class TrainingAdmin( admin.ModelAdmin ):
+    list_display = ( 'description', 'user', 'date' )
+    list_filter = ( 'user', 'date' )
+
+class SetAdmin( admin.ModelAdmin ):
+    pass
+
+class ExerciseAdmin( admin.ModelAdmin ):
+    pass
+
+admin.site.register( Type, TypeAdmin )
+admin.site.register( Muscle, MuscleAdmin )
+admin.site.register( ExerciseTemplate, ExerciseTemplateAdmin )
+admin.site.register( Training, TrainingAdmin )
+admin.site.register( Set, SetAdmin )
+admin.site.register( Exercise, ExerciseAdmin )
