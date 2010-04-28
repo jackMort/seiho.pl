b2c147ed64c4c2fa92e837d88ef737c2b06e1244
sherkhan <sherkhan@rootnode.net>
2010-04-28 11:15:44 +0200

    refaktoryzacja kodu

--- gym/media/js/application.exercises.js
+++ gym/media/js/application.exercises.js
@@ -6,7 +6,7 @@ Seiho.gym.exercise.MainPanel = Ext.extend( Ext.grid.GridPanel, {
 		var xg = Ext.grid;
 
 		var experienceStore = new Ext.data.GroupingStore({
-            groupField: 'date',
+			groupField: 'date',
 			proxy: new Ext.data.DirectProxy({
 				directFn: django.exercises.list
 			}),
@@ -16,7 +16,7 @@ Seiho.gym.exercise.MainPanel = Ext.extend( Ext.grid.GridPanel, {
 					{name: 'id'},
 					{name: 'template_name'},
 					{name: 'template_type'},
-       				{name: 'date', type: 'date', dateFormat: 'Y-m-d h:i:s'},
+					{name: 'date', type: 'date', dateFormat: 'Y-m-d h:i:s'},
 				]
 			})
 		});
@@ -24,20 +24,20 @@ Seiho.gym.exercise.MainPanel = Ext.extend( Ext.grid.GridPanel, {
 		Ext.apply( this, {
 			store: experienceStore,
 			columns: [
-					{id:'tn',header: "Nazwa", width: 60, sortable: true, dataIndex: 'template_name'},
-					{header: "Typ", width: 20, sortable: true, dataIndex: 'template_type'},
-					{header: "Data", width: 20, sortable: true, renderer: Ext.util.Format.dateRenderer('Y-m-d'), dataIndex: 'date'}
-				],
+				{id:'tn',header: "Nazwa", width: 60, sortable: true, dataIndex: 'template_name'},
+				{header: "Typ", width: 20, sortable: true, dataIndex: 'template_type'},
+				{header: "Data", width: 20, sortable: true, renderer: Ext.util.Format.dateRenderer('Y-m-d'), dataIndex: 'date'}
+			],
 
-				view: new Ext.grid.GroupingView({
-					forceFit:true,
-					hideGroupedColumn: true,
-					groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "Elementów" : "Element"]})'
-				}),
-				// frame:true,
-				title: 'Dziennik treningowy',
-				iconCls: 'icon-grid',
-				tbar: [
+			view: new Ext.grid.GroupingView({
+				emptyText: 'Brak danych treningowych. Zacznij ćwiczyć ...',
+				forceFit:true,
+				hideGroupedColumn: true,
+				groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "Elementów" : "Element"]})'
+			}),
+			title: 'Dziennik treningowy',
+			iconCls: 'icon-grid',
+			tbar: [
 				' ', { xtype: 'textfield', width: 250, emptyText: 'Wyszukaj w dzienniku ...' },
 				'->', 
 				{ iconCls: 'icon-page_white', text: 'dodaj' }, '-', 
