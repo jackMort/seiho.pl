87aaabe6810ee63daf5ac6e17fbaabab8b70df45
sherkhan <sherkhan@rootnode.net>
2010-04-28 22:09:58 +0200

    dodane metody __str__ dla reszty klas

--- gym/exercises/models.py
+++ gym/exercises/models.py
@@ -33,12 +33,21 @@ class Set( models.Model ):
     distance = models.FloatField( null=True )
     time = models.TimeField( null=True )
 
+    def __str__( self ):
+	return "n: %d, r: %d, m: %f, d: %f, t: %s" % ( self.number, self.reps, self.mass, self.distance, self.time )
+
 class Exercise( models.Model ):
     template = models.ForeignKey( ExerciseTemplate )
     sets = models.ManyToManyField( Set )
+    
+    def __str__( self ):
+	return "%s [%d]" % ( self.template.name, len( self.sets.all() ) )
 
 class Training( models.Model ):
     user = models.ForeignKey( User )
     date = models.DateTimeField()
     description = models.TextField()
     exercises = models.ManyToManyField( Exercise, related_name='training' )
+
+    def __str__( self ):
+	return "%s [%s] - %s" % ( self.user, self.date, self.description )
