c11aed668ef357a9958417dbd1a50d67a8279673
sherkhan <sherkhan@rootnode.net>
2010-05-06 10:31:09 +0200

    dodanie możliwości ukrycia, dodania informacji o seriach

--- gym/media/js/application.exercises.js
+++ gym/media/js/application.exercises.js
@@ -36,23 +36,26 @@ Seiho.gym.exercise.MainPanel = Ext.extend( Ext.grid.GridPanel, {
 				hideGroupedColumn: true,
 				groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "Elementów" : "Element"]})',
 				enableRowBody: true,
+				showPreview: true,
 				getRowClass: function( record, rowIndex, rp, ds ) {
-					var sets = Ext.util.JSON.decode( record.get( 'sets' ) );
-					// TODO: template
-					var items = '';
-					for( var i = 0, len = sets.length; i < len; i++ ) {
-						var mass = sets[i].fields.mass;
-						var time = sets[i].fields.time;
-						var reps = sets[i].fields.reps;
-						var number = sets[i].fields.number;
-						var distance = sets[i].fields.distance;
-
-						var info = ( reps > 0 ? '<span class="biggest">' + reps + '</span> x ' : '' ) + ( mass > 0 ? '<span class="biggest">' + mass + '</span> kg ' : '' )
-
-						items += '<div class="set round-corners"><span class="number round-corners">' + number + '</span> ' + info + '</div>'
+					if( this.showPreview ) {
+						var sets = Ext.util.JSON.decode( record.get( 'sets' ) );
+						// TODO: template
+						var items = '';
+						for( var i = 0, len = sets.length; i < len; i++ ) {
+							var mass = sets[i].fields.mass;
+							var time = sets[i].fields.time;
+							var reps = sets[i].fields.reps;
+							var number = sets[i].fields.number;
+							var distance = sets[i].fields.distance;
+	
+							var info = ( reps > 0 ? '<span class="biggest">' + reps + '</span> powtórzeń' : '' ) + ( mass > 0 ? ' x <span class="biggest">' + mass + '</span> kg ' : '' ) + ( distance > 0 ? ' <span class="biggest">' + distance + '</span> m' : '' ) + ( time != '00:00:00' ? ' <span class="biggest">' + time + '</span>' : '' )
+	
+							items += '<div class="set round-corners"><span class="number round-corners">' + number + '</span> ' + info + '</div>'
+						}
+	
+						rp.body = '<div class="exercise-bodyrow">' + items + '</div>'
 					}
-
-					rp.body = '<div class="exercise-bodyrow">' + items + '</div>'
 					return 'x-grid3-row-expanded'
 				}
 			}),
@@ -64,6 +67,8 @@ Seiho.gym.exercise.MainPanel = Ext.extend( Ext.grid.GridPanel, {
 				{ iconCls: 'icon-page_white', text: 'dodaj' }, '-', 
 				{ iconCls: 'icon-page_white_edit', text: 'edytuj', disabled: true },
 				{ iconCls: 'icon-delete', text: 'usuń', disabled: true },
+				'-',
+				{ iconCls: 'icon-common', text: 'szczegóły serii', enableToggle: true, pressed: true, toggleHandler: this.toggleSeries, scope: this },
 				' '
 			]
 		});
@@ -71,6 +76,11 @@ Seiho.gym.exercise.MainPanel = Ext.extend( Ext.grid.GridPanel, {
 		Seiho.gym.exercise.MainPanel.superclass.initComponent.apply( this, arguments );
 
 		experienceStore.load()
+	},
+	toggleSeries: function( btn, pressed ) {
+		var view = this.getView();
+		view.showPreview = pressed;
+		view.refresh();
 	},
 	addExercise: function() {
 		// TODO implementacja
