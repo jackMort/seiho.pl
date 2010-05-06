d3fd4bcfd56fc26931a67f9a2b73264938a55c2e
Lech Twaróg <lech.twarog@gmail.com>
2010-05-06 11:59:39 +0200

    dodanie lokalizatora użytkowników ...

--- gym/media/js/application.js
+++ gym/media/js/application.js
@@ -4,7 +4,10 @@ seiho.gym.App = function() {
 	return {
 		init: function() {
 			Ext.QuickTips.init();
-			
+
+			// lokalizacja użytkownika
+			Ext.get( 'user-location' ).on( 'click', this.shareLocation, this);
+
 			// mapowoania akcji ...
 			new Ext.KeyMap( document, {
 				alt: true,
@@ -68,7 +71,65 @@ seiho.gym.App = function() {
 			});
 
 			this.loginDialog.show();
-		}		
+		},
+		shareLocation: function( e, t ) {			
+			e.preventDefault();
+		
+			if( this.shareLocationWindow ) {
+				this.shareLocationWindow.toFront();
+				this.shareLocationWindow.getEl().frame();
+
+				return false;
+			}
+
+			this.shareLocationWindow = new Ext.Window({
+				layout: 'fit',
+				iconCls: 'icon-map',
+				title: 'Twoja lokalizacja',
+				width: 600,
+				height: 200,
+				x: e.getXY()[0] - 600,
+				y: e.getXY()[1] + 35,
+				items: new Ext.ux.GMapPanel({
+					listeners: {
+						render: {
+							fn: function( panel ) {
+								panel.getEl().mask( 'Trwa pobieranie danych o lokacji ...', 'x-mask-loading' );
+								// ..
+								if ( navigator.geolocation ) {
+									navigator.geolocation.getCurrentPosition( function( pos ) {
+										var point = new GLatLng( pos.coords.latitude, pos.coords.longitude );
+										// ..
+										panel.getMap().setCenter( point, 13 )
+										panel.addMarker( point, { title: 'Tu jesteś koksie ...' } )
+										// ..
+										panel.getEl().unmask();
+									});
+								}
+							},
+							delay: 2000						
+						}
+					},
+					xtype: 'gmappanel',
+					zoomLevel: 5,
+					gmapType: 'map',
+					mapConfOpts: ['enableScrollWheelZoom', 'enableDoubleClickZoom', 'enableDragging' ],
+					mapControls: ['GSmallMapControl', 'GmapTypeControl', 'NonExistantControl'],
+					setCenter: {
+						lat: 52,
+						lng: 19
+					}
+				}),
+				listeners: {
+					close: {
+						fn: function() {
+							this.shareLocationWindow = undefined
+						},
+						scope: this
+					}
+				}
+			}).show( t )			
+		}
 	}
 }();
 
